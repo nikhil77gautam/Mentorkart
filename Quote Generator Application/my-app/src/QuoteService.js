46291299ae2b148ff7877434839fa2563cc7f5dc
Nikhil Gautam <135546239+nikhil77gautam@users.noreply.github.com>
2024-02-26 14:33:52 +0530

    Update QuoteService.js

--- Quote Generator Application/my-app/src/QuoteService.js
+++ Quote Generator Application/my-app/src/QuoteService.js
@@ -1,18 +1,1 @@
-// import axios from 'axios';
-// const QUOTABLE_API_URL = 'https://api.quotable.io/random';
 
-// const getRandomQuote = async () => {
-//   try {
-//     const response = axios.get(QUOTABLE_API_URL);
-//     const { content, author } = response.data;
-//     return { content, author };
-//   } catch (error) {
-//     throw new Error('Failed to fetch quote');
-//   }
-// };
-
-// const QuoteService = {
-//   getRandomQuote,
-// };
-
-// export default QuoteService;
